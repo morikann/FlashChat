9badbf983133d3bb78802b34bf45005ca0e6f8ef
morikann <mrknt0088@gmail.com>
2021-07-05 16:26:26 +0900

    add tableview and firestore

---
+++ Flash Chat iOS13/Controllers/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 森寛太 on 2021/07/04.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
